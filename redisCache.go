8435483aec685057f9dab92160a666fd250dc136
Itay <itayan116@gmail.com>
2021-03-20 14:33:59 +0200

    implementation done

--- redisCache.go
+++ redisCache.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -14,34 +15,55 @@ const (
 
 // RedisCache is a client that implements Cache interface.
 type RedisCache struct {
-	client   *redis.Client
-	keysList []string
+	// This dictionary is maintained in order to keep track of this
+	// instance's keys for functions like clear.
+	keysSet map[string]struct{}
+
+	// Holds the channels that stop the auto removal routines.
+	removeChannels map[interface{}]*cacheChannel
+
+	client *redis.Client
+
+	mutex sync.Mutex
 }
 
 var _ (ExpiringCache) = (*RedisCache)(nil)
 
+// --------------------------------------------------------------------------
+
 // NewRedisCache creates and returns a reference to a RedisCache instance.
 func NewRedisCache(address, password string, db int) *RedisCache {
 	return &RedisCache{
-		redis.NewClient(&redis.Options{
+		keysSet:        map[string]struct{}{},
+		removeChannels: map[interface{}]*cacheChannel{},
+		client: redis.NewClient(&redis.Options{
 			Addr:     address,
 			Password: password,
 			DB:       db,
 		}),
-		[]string{},
 	}
 }
 
-func (r *RedisCache) store(key, val interface{}) error {
+func (r *RedisCache) store(key, val interface{}, ttl time.Duration) error {
 	strKey := fmt.Sprintf("%v", key)
-	status := r.client.Set(context.TODO(), strKey, val, 0)
+	status := r.client.Set(context.TODO(), strKey, val, ttl).Val()
 
-	r.keysList = append(r.keysList, strKey)
+	if status != "OK" {
+		return newError(errorTypeRedisError, fmt.Sprintf("could not store key %v", strKey))
+	}
 
+	r.keysSet[strKey] = struct{}{}
+
+	return nil
 }
 
 func (r *RedisCache) get(key interface{}) (interface{}, error) {
 	strKey := fmt.Sprintf("%v", key)
+
+	if _, ok := r.keysSet[strKey]; !ok {
+		return nil, newError(errorTypeDoesNotExist,
+			fmt.Sprintf("cannot get key %v", strKey))
+	}
 
 	val, err := r.client.Get(context.TODO(), strKey).Result()
 	if err == redis.Nil {
@@ -60,6 +82,11 @@ func (r *RedisCache) get(key interface{}) (interface{}, error) {
 func (r *RedisCache) remove(key interface{}) error {
 	strKey := fmt.Sprintf("%v", key)
 
+	if _, ok := r.keysSet[strKey]; !ok {
+		return newError(errorTypeDoesNotExist,
+			fmt.Sprintf("cannot remove key %v", strKey))
+	}
+
 	res := r.client.Del(context.TODO(), strKey).Val()
 	if res < 1 {
 		return newError(errorTypeDoesNotExist,
@@ -70,21 +97,11 @@ func (r *RedisCache) remove(key interface{}) error {
 }
 
 func (r *RedisCache) replace(key, val interface{}) error {
-	err := r.remove(key)
-	if err != nil {
-		return err
-	}
-
-	err = r.store(key, val)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return r.store(key, val, 0)
 }
 
 func (r *RedisCache) clear() error {
-	for key := range r.keysList {
+	for key := range r.keysSet {
 		err := r.remove(key)
 		if err != nil {
 			return err
@@ -95,76 +112,152 @@ func (r *RedisCache) clear() error {
 }
 
 func (r *RedisCache) keys() ([]interface{}, error) {
-	return []interface(r.keysList), nil
+	keys := []interface{}{}
+
+	for key := range r.keysSet {
+		keys = append(keys, key)
+	}
+
+	return keys, nil
 }
 
 func (r *RedisCache) storeWithExpiration(key, val interface{}, ttl time.Duration) error {
+	if ttl <= 0 {
+		return newError(errorTypeNonPositivePeriod, "period must be greater than zero")
+	}
+
+	r.store(key, val, ttl)
+
+	r.createExpirationRoutine(key, ttl)
+
 	return nil
 }
 
 func (r *RedisCache) replaceWithExpiration(key, val interface{}, ttl time.Duration) error {
+	if ttl <= 0 {
+		return newError(errorTypeNonPositivePeriod, "period must be greater than zero")
+	}
+
+	err := r.storeWithExpiration(key, val, ttl)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (r *RedisCache) expire(key interface{}, ttl time.Duration) error {
+	if ttl <= 0 {
+		return newError(errorTypeNonPositivePeriod, "period must be greater than zero")
+	}
+
+	strKey := fmt.Sprintf("%v", key)
+
+	res := r.client.Expire(context.TODO(), strKey, ttl).Val()
+	if !res {
+		return newError(errorTypeRedisError, fmt.Sprintf("could not expire key %v", strKey))
+	}
+
+	r.createExpirationRoutine(key, ttl)
+
 	return nil
 }
 
-func (r *RedisCache) storeWithUpdate(key, initialValue interface{},
-	updateFunc func(currValue interface{}) interface{},
-	period time.Duration) error {
-	return nil
-}
+func (r *RedisCache) createExpirationRoutine(key interface{}, ttl time.Duration) {
+	c := newCacheChannel()
+	r.removeChannels[key] = c
 
-func (r *RedisCache) replaceWithUpdate(key, initialValue interface{},
-	updateFunc func(currValue interface{}) interface{},
-	period time.Duration) error {
-	return nil
+	expireSignalerRoutine := func(c *cacheChannel) {
+		<-time.After(ttl)
+		c.signal(proceed)
+	}
+
+	expireRoutine := func(key interface{}, c *cacheChannel) {
+		msg, ok := <-c.c
+		if !ok || msg == abort {
+			return
+		}
+
+		r.mutex.Lock()
+		defer r.mutex.Unlock()
+
+		delete(r.keysSet, fmt.Sprintf("%v", key))
+	}
+
+	go expireSignalerRoutine(c)
+	go expireRoutine(key, c)
 }
 
 // --------------------------------------------------------------------------
 
 // Store permanent value in redis.
 func (r *RedisCache) Store(key, val interface{}) error {
-	return r.store(key, val)
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	return r.store(key, val, 0)
 }
 
 // Get a value from redis.
 func (r *RedisCache) Get(key interface{}) (interface{}, error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.get(key)
 }
 
 // Remove a value from redis.
 func (r *RedisCache) Remove(key interface{}) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.remove(key)
 }
 
 // Replace an existing value in redis.
 func (r *RedisCache) Replace(key, val interface{}) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.replace(key, val)
 }
 
 // Clear all values that maintained by this RedisCache instance.
 func (r *RedisCache) Clear() error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.clear()
 }
 
 // Keys return all keys that maintained by this RedisCache instance.
 func (r *RedisCache) Keys() ([]interface{}, error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.keys()
 }
 
 // StoreWithExpiration stores a key-value pair in redis for limited time.
 func (r *RedisCache) StoreWithExpiration(key, val interface{}, ttl time.Duration) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.storeWithExpiration(key, val, ttl)
 }
 
 // ReplaceWithExpiration replaces a key-value pair in redis for limited time.
 func (r *RedisCache) ReplaceWithExpiration(key, val interface{}, ttl time.Duration) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.replaceWithExpiration(key, val, ttl)
 }
 
 // Expire a key-value pair.
 func (r *RedisCache) Expire(key interface{}, ttl time.Duration) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
 	return r.expire(key, ttl)
 }
